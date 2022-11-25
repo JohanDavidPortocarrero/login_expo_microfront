c8b21a7997361e9c8c7f8857975c88d6c11cdce3
J. David <portocarrero.johan@correounivalle.edu.co>
2022-11-25 15:59:14 -0500

    Login / Front - Back v2

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueSweetalert2 from 'vue-sweetalert2';
+//import VueSweetalert2 from 'vue-sweetalert2';
 import App from './App'
 import router from './router'
 
 //import BootstrapVue from 'bootstrap-vue'
-Vue.use(VueSweetalert2);
+//Vue.use(VueSweetalert2);
 
 //import 'bootstrap/js/dist/util';
 //import 'bootstrap/js/dist/dropdown';
